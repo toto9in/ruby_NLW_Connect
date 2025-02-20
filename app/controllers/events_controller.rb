6804a8599b4f8a82b6998fce5f41e64bc6b186ee
toto9 <aaba03@gmail.com>
2025-02-20 20:48:29 -0300

    refactor: put EventsCreatorService in module Events in services

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,7 +2,7 @@ class EventsController < ApplicationController
   def create
     params = validate_params!(CreateEventValidator.new, create_event_params)
 
-    response = EventsCreatorService.new.create(params[:name])
+    response = Events::EventsCreatorService.new.create(params[:name])
 
     render json: response[:body], status: response[:status_code]
   end
@@ -22,6 +22,36 @@ class EventsController < ApplicationController
       result.to_h
     else
       raise InvalidParamsError.new(result.errors.to_h)
+    end
+  end
+end
+
+module Events
+  class EventsController < ApplicationController
+    def create
+      params = validate_params!(CreateEventValidator.new, create_event_params)
+
+      response = EventsCreatorService.new.create(params[:name])
+
+      render json: response[:body], status: response[:status_code]
+    end
+
+
+    private
+
+    def create_event_params
+      params.expect(data: [ :name ]).to_h
+    end
+
+
+    def validate_params!(validator, params)
+      result = validator.call(params)
+
+      if result.success?
+        result.to_h
+      else
+        raise InvalidParamsError.new(result.errors.to_h)
+      end
     end
   end
 end
