c0349863c7ecc3476418cfdba8571d001ab97beb
toto9 <aaba03@gmail.com>
2025-02-20 23:36:20 -0300

    fix: fix model validations

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,14 +3,4 @@ class Event < ApplicationRecord
   has_many :events_link, dependent: :destroy
 
   validates :name, presence: true
-  validates :email, presence: true
-  validates :link, presence: false
-  validates :event_id, presence: true
-  validate  :event_must_exist
-
-  private
-
-  def event_must_exist
-    errors.add(:event_id, "must exist") unless Event.exists?(self.event_id)
-  end
 end
--- app/models/subscriber.rb
+++ app/models/subscriber.rb
@@ -1,4 +1,16 @@
 class Subscriber < ApplicationRecord
   belongs_to :event
   has_many :events_link, dependent: :destroy
+
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :link, presence: false
+  validates :event_id, presence: true
+  validate  :event_must_exist
+
+  private
+
+  def event_must_exist
+    errors.add(:event_id, "must exist") unless Event.exists?(self.event_id)
+  end
 end
